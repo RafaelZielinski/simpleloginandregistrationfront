a2250237c5fea2ac5595498d5452c80b323ea47f
Rafael <rafekzielinski@wp.pl>
2024-04-22 22:58:01 +0200

    adding bootstrap icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simpleloginandregistrationapp';
+  title = 'Simple Login and Registration app';
 }
