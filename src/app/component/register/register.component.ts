04eb5ad1ad06551857e1913db7e6b640fbd967fd
Rafael <rafekzielinski@wp.pl>
2024-04-28 15:16:34 +0200

    adding register component with methods

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
+import { DataState } from 'src/app/enum/datastate.enum';
+import { RegisterState } from 'src/app/interface/appstates';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  readonly DataState = DataState;
+  registerState$ : Observable<RegisterState> = of({ dataState: DataState.LOADED, registerSuccess:true});
+  private isClosed = new BehaviorSubject<boolean>(false);
+  isClosed$ = this.isClosed.asObservable();
+
+
+  constructor(private userService: UserService) {
+
+  }
+  register(registerForm: NgForm):void {
+    this.registerState$ = this.userService.save$(registerForm.value)
+    .pipe(
+      map(response => {
+        registerForm.reset();
+        return {dataState: DataState.LOADED, registerSuccess: true, message: response.message};
+      }),
+      startWith({dataState: DataState.LOADING, registerSuccess: false}),
+      catchError((error: string) => {
+        this.isClosed.next(true);
+        return of({ dataState: DataState.ERROR, registerSuccess: false, error: error})
+      })
+    );
+  }
+
+  createAccountForm():void {
+    this.registerState$ = of({dataState: DataState.LOADED, registerSuccess: false});
+  }
 
 }
